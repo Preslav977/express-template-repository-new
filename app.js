9d6a33bf1a042974e0ab137c2685dcd75dce138b
Preslav977 <p.cvetanow@gmail.com>
2024-10-09 09:13:27 +0300

    Readded the deleted controllers folder, removed some code lines which didnt allow the server to start. Added script to start the server.

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-});
-
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Express app - listening on port ${PORT}!`));
 
