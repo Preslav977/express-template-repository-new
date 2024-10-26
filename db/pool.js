80481e8fa44b1fe4f35ba9e20e63642111b1b5a8
Preslav977 <p.cvetanow@gmail.com>
2024-10-26 12:35:36 +0300

    Added more dev dependecies and added ssl in the pool and populatedb file

--- db/pool.js
+++ db/pool.js
@@ -2,4 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
--- db/populatedb.js
+++ db/populatedb.js
@@ -4,6 +4,9 @@ async function main() {
   console.log("seeding...");
   const client = new Client({
     connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
   await client.connect();
   await client.query(SQL);
